810048e05df20ed1b751ef216ff1880f42a346a9
abdallah abusnineh <124106124+Abdallahabusnineh@users.noreply.github.com>
2024-08-06 02:28:13 +0300

    Update grow_your_business_header.dart

--- lib/view/grow_your_busniess/widget/grow_your_business_header.dart
+++ lib/view/grow_your_busniess/widget/grow_your_business_header.dart
@@ -14,78 +14,152 @@ class GrowYourBusinessHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return IsResponsive.isWebScreen(context)?Row(
       children: [
         Expanded(
-
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: 5.h,),
-              Text.rich(
-                TextSpan(
-                  text: title,
-                  style: TextStyle(
-                    fontSize: 25.sp,
-                    overflow: TextOverflow.ellipsis,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+              FittedBox(
+                child: Text.rich(
+                  TextSpan(
+                    text: title,
+                    style: TextStyle(
+                      fontSize: 25.sp,
+                      overflow: TextOverflow.ellipsis,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    children: [
+                      TextSpan(
+                        style: TextStyle(
+                          color: Colors.greenAccent.shade400,
+                        ),
+                        text: underlineTitle,
+                      )
+                    ],
                   ),
-                  children: [
-                    TextSpan(
-                      style: TextStyle(
-                        color: Colors.greenAccent.shade400,
-                      ),
-                      text: underlineTitle,
-                    )
-                  ],
                 ),
               ),
               SizedBox(
                 height: 2.h,
               ),
-              Text(
-                subtitle,
-                style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    fontSize: 17.sp,
-                    color: Colors.white,
-                    overflow: TextOverflow.ellipsis),
-                maxLines: 2,
+              FittedBox(
+                child: Text(
+                  subtitle,
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 17.sp,
+                      color: Colors.white,
+                      overflow: TextOverflow.ellipsis),
+                  maxLines: 2,
 
+                ),
               ),
               SizedBox(
                 height: 5.h,
               ),
-             Row(
-                 children: [
-                 ElevatedButton(
-                     style: ElevatedButton.styleFrom(backgroundColor: Colors.greenAccent.shade400,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.sp))),
-                     onPressed: (){}, child: Text('Apply Now',style: TextStyle(color: Colors.black),)),
-SizedBox(width: 4.w,),
-                 TextButton(onPressed: (){}, child: Text('Contact Us',style: TextStyle(color: Colors.greenAccent.shade400),)),
+              FittedBox(
+                child: Row(
+                  children: [
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.greenAccent.shade400,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.sp))),
+                        onPressed: (){}, child: Text('Apply Now',style: TextStyle(color: Colors.black),)),
+                    SizedBox(width: 4.w,),
+                    TextButton(onPressed: (){}, child: Text('Contact Us',style: TextStyle(color: Colors.greenAccent.shade400),)),
 
-               ],
-             )
+                  ],
+                ),
+              )
             ],
           ),
 
         ),
         Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.sp),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/busniess_women.jpeg'),
+                )
+            ),
+          ),
+        ),
+
+      ],
+    ):Column(
+      children: [
+        Expanded(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-             Expanded(
-               child: Container(
-                 decoration: BoxDecoration(
-                   borderRadius: BorderRadius.circular(20.sp),
-                   image: DecorationImage(
-                     fit: BoxFit.fill,
-                     image: AssetImage('assets/busniess_women.jpeg'),
-                   )
-                 ),
-               ),
-             )
+              SizedBox(height: 5.h,),
+              FittedBox(
+                child: Text.rich(
+                  TextSpan(
+                    text: title,
+                    style: TextStyle(
+                      fontSize: 25.sp,
+                      overflow: TextOverflow.ellipsis,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    children: [
+                      TextSpan(
+                        style: TextStyle(
+                          color: Colors.greenAccent.shade400,
+                        ),
+                        text: underlineTitle,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 2.h,
+              ),
+              FittedBox(
+                child: Text(
+                  subtitle,
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 17.sp,
+                      color: Colors.white,
+                      overflow: TextOverflow.ellipsis),
+                  maxLines: 2,
+
+                ),
+              ),
+              SizedBox(
+                height: 5.h,
+              ),
+              FittedBox(
+                child: Row(
+                  children: [
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.greenAccent.shade400,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.sp))),
+                        onPressed: (){}, child: Text('Apply Now',style: TextStyle(color: Colors.black),)),
+                    SizedBox(width: 4.w,),
+                    TextButton(onPressed: (){}, child: Text('Contact Us',style: TextStyle(color: Colors.greenAccent.shade400),)),
+
+                  ],
+                ),
+              )
             ],
+          ),
+
+        ),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.sp),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/busniess_women.jpeg'),
+                )
+            ),
           ),
         ),
 
